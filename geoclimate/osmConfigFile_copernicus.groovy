5a1651497f474316709519812c8e2e5cd0d7e7cb
erenault <e.renault.ubs@gmail.com>
2020-11-28 13:50:45 +0100

    jsonSlurper

--- geoclimate/osmConfigFile_copernicus.groovy
+++ geoclimate/osmConfigFile_copernicus.groovy
@@ -253,7 +253,8 @@ def createOSMConfigFile(def osmParameters, def directory){
 def createOSMFiltersList(def osmFiltersFile, def directory) {
     def jsonFile =  new File(directory+osmFiltersFile)
     if (jsonFile.exists() && jsonFile.length()>0) {
-        def data = new JsonSlurper().parse(jsonFile)
+        def jsonSlurper = new jsonSlurper()
+        def data = jsonSlurper.parse(jsonFile)
         def osmFilters = jsonSlurper.parseText(JsonOutput.toJson(data))
         def osmFiltersList = []
         N = osmFilters["N"]
